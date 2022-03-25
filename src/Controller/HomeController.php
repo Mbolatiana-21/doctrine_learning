c345733c66df391bbbd8d84290cfbbb0e3a47b3d
unknown <fenosoaranaivozanany@gmail.com>
2022-03-25 20:39:52 +0100

    update 25_03_22

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Home;
+
 use App\Form\Type\HomeType;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController
 {
@@ -16,19 +18,44 @@ class HomeController extends AbstractController
      * @Route("/homepage")
      */
     
-    public function new(Request $request) : Response
+    public function new(Request $request ,ManagerRegistry $doctrine) : Response
     {
         $home = new Home();
-        $home -> setArticlename('Veste');
-        $home -> setQuantity('4');
-        $home -> setReference('VST'); 
-
+       
         $form = $this->createForm(HomeType::class, $home);
-         
-        return $this ->render('homepage/home.html.twig',[
+
+        /* ajout donne dans la base */
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($home);
+            $em->flush();
+          
+        }
+        
+        //affichage donne
+        $home = $doctrine->getRepository(Home::class)->findAll();
+
+        return $this ->renderForm('homepage/home.html.twig',[
             'form' => $form,
+            'home' => $home
         ]);
     } 
+
+   /* public function show(ManagerRegistry $doctrine): Response
+    {
+        $home = $doctrine->getRepository(Home::class)->findAll();
+     
+        if ($home) {
+            throw $this->createNotFoundException(
+                ' product find' 
+            );
+        }
+        return $this->render('homepage/home.html.twig' ,
+         ['home' => $home]);
+    }
+
 
   /*
     public function Helloworld(): Response
